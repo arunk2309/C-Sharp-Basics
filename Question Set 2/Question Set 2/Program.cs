e305032285da1170d8ddbc86622d31addf299737
arunk2309 <arunachalamkathiresan.95@gmail.com>
2023-12-13 15:47:52 +0530

    QS2 Second Part

--- Question Set 2/Question Set 2/Program.cs
+++ Question Set 2/Question Set 2/Program.cs
@@ -59,16 +59,109 @@ namespace Question_Set_2
             //    Console.WriteLine("Entered Number is Odd Number");
             //}
             // NO 5
-            Console.WriteLine("Enter the Year");
-            int year = int.Parse(Console.ReadLine());
-            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
-            {
-                Console.WriteLine("Is a Leap Year", year);
-            }
+            //Console.WriteLine("Enter the Year");
+            //int year = int.Parse(Console.ReadLine());
+            //if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            //{
+            //    Console.WriteLine("Is a Leap Year", year);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Is not a Leap year", year);
+            //}
+            // NO 6
+            //Console.WriteLine("Enter the Age");
+            //int Age = int.Parse(Console.ReadLine());
+            //if (Age >= 18)
+            //{
+            //    Console.WriteLine("HE or She is Eligible to cast her vote");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("He or She is not Eligible to cast her vote");
+            //}
+
+            //NO 7
+            //Console.WriteLine("Enter the Height of the Person");
+            //int height = int.Parse(Console.ReadLine());
+            //if (height > 220)
+            //{
+            //    Console.WriteLine("You are Classifed as a Tall");
+            //}
+            //else if (height < 160)
+            //{
+            //    Console.WriteLine("You are Classified as a Short");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are Classified as a Medium");
+            //}
+
+            // NO 8
+            //Console.WriteLine("Enter the Value for A");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of B");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of C");
+            //int c = int.Parse(Console.ReadLine());
+            //int result = LargestNumber(a, b, c);
+            //Console.WriteLine("The Largest Among Three Numbers is: " + result);
+
+            // NO 9
+            //Console.WriteLine("Enter the Value of A");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the value of B");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of C");
+            //int c = int.Parse(Console.ReadLine());
+            //int result = SmallestNumber(a, b, c);
+            //Console.WriteLine("The Smallest amount Three Number is: " + result);
+
+            // NO 10
+            Console.WriteLine("Enter the Value of A");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of B");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of C");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Value of D");
+            int d = int.Parse(Console.ReadLine());
+            int result = LargestFourNumber(a, b, c, d);
+            Console.WriteLine("The Largest Among Four Number is: " + result);
+        }
+
+        // No 8
+        public static int LargestNumber(int a, int b, int c)
+        {
+            if (a >= b && a >= c)
+                return a;
+            else if (b >= a && b >= c)
+                return b;
             else
-            {
-                Console.WriteLine("Is not a Leap year", year);
-            }
+                return c;
+        }
+        // NO 10
+        public static int SmallestNumber(int a, int b, int c)
+        {
+            if (a <= b && a <= c)
+                return a;
+            else if (b <= a && b <= c)
+                return b;
+            else
+                return c;
+        }
+
+        // No 10
+        public static int LargestFourNumber(int a, int b, int c, int d)
+        {
+            if (a >= b && a >= c && a >= d)
+                return a;
+            else if (b >= a && b >= c && b >= d)
+                return b;
+            else if (c >= a && c >= b && c >= d)
+                return c;
+            else
+                return d;
         }
     }
 }
