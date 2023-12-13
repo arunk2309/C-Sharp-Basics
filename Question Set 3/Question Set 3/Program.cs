76be6f0954a4ab25008832a4424e2ce569a7de7c
arunk2309 <arunachalamkathiresan.95@gmail.com>
2023-12-13 21:18:21 +0530

    QS3 Second Part

--- Question Set 3/Question Set 3/Program.cs
+++ Question Set 3/Question Set 3/Program.cs
@@ -57,20 +57,75 @@ namespace Question_Set_3
             //    }
             //}
             // NO 5
-            Console.WriteLine("Enter the Value of M");
-            int M = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of M");
+            //int M = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of N");
+            //int N = int.Parse(Console.ReadLine());
+            //if (M < N)
+            //{
+            //    for (int i = M; i <= N; i++)
+            //    {
+            //        if (i % 2 == 0)
+            //        {
+            //            Console.WriteLine(i);
+            //        }
+            //    }
+            //}
+            // NO 6
+            //Console.WriteLine("Enter the Value of M");
+            //int M = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the Value of N");
+            //int N = int.Parse(Console.ReadLine());
+            //for (int i = M; i <= N; i++)
+            //{
+            //    if (i % 2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            // NO 7
+            //Console.WriteLine("Enter the Value of N");
+            //int N = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    if (i % N == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            // NO 8
+            //Console.WriteLine("Enter the Value of N");
+            //int N = int.Parse(Console.ReadLine());
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(N + " X " + i + " = " + (N * i));
+            //}
+            // NO 9
+            //Console.WriteLine("Multiplication Tables");
+            //for (int i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("Table for " + i);
+            //        for (int j = 1; j <= 20; j++)
+            //        {
+            //            int result = i * j;
+            //            Console.WriteLine(+i + " X " + j + " = " + result);
+            //        }
+            //}
+            // NO 10
             Console.WriteLine("Enter the Value of N");
-            int N = int.Parse(Console.ReadLine());
-            if (M < N)
+            int n = int.Parse(Console.ReadLine());
+            int sum = CalculateSum(n);
+            Console.WriteLine("The sum of all number between 1 and " + n + " is " + sum);
+    }
+        // No 10
+        public static int CalculateSum(int n)
+        {
+            int sum = 0;
+            for (int i = 1; i <= n; i++)
             {
-                for (int i = 1; i <= N; i++)
-                {
-                    if (i % 2 == 0)
-                    {
-                        Console.WriteLine(i);
-                    }
-                }
+                sum += i;
             }
+            return sum;
         }
     }
 }
